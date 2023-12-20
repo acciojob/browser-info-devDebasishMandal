ebe0dfff96a04547d4146a1a394eafdc5d6023d9
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2023-12-20 22:40:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let info=document.getElementById("browser-info");
+let browser=navigator.appName;
+let version=navigator.appVersion;
+info.innerText=`You are using ${browser}${version} version.`;
